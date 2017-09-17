4db02beac6e6f495f46ade88395f3828f1f04b1f
Lewis Treacy <lewis.treacy16@imperial.ac.uk>
2017-09-17 16:30:51 +0100

    Finished key expansion implementation

--- aes.go
+++ aes.go
@@ -1,23 +1,41 @@
-// Advanced Encryption Standard implementation
+// Advanced Encryption Standard implementation.
 package aes
 
-func expandKey(key []byte) []byte {
-	var exKey = make([]byte, 256)
-	return exKey
+// Expands key according to the Rijndael key schedule.
+// Pre: key is 16, 24 or 32 bytes in length
+func expandKey(key []byte) []uint32 {
+	nk := len(key) / 4
+	w := make([]uint32, 4*(rounds[len(key)] + 1))
+	// Begining of the expanded key is the same
+	for i := 0; i < nk; i++ {
+		w[i] = uint32(key[4*i])<<24 | uint32(key[4*i+1])<<16 | uint32(key[4*i+2])<<8 | uint32(key[4*i+3])
+	}
+
+	for j := nk; j < len(w); j++ {
+		a := w[j-1]
+		if j % nk == 0 {
+			a = subw(rotw(a)) ^ rcon(j/nk)
+		} else if nk == 8 && j % nk == 4 {
+			a = subw(a)
+		}
+		w[j] = w[j-nk] ^ a
+	}
+
+	return w
 }
 
-// Rotate 32 bit word left by 1 byte
+// Rotate word left 1 byte.
 func rotw(w uint32) uint32 {
 	return (w<<8) | (w>>24)
 }
 
-// Gets S-Box substitution value for a byte
-func sub(b byte) byte {
-	return sbox[uint8(b)]
+// Substitutes each byte of word w according to Rijndael's S-box.
+func subw(w uint32) uint32 {
+	return uint32(sbox[w>>24])<<24 | uint32(sbox[(w>>16)&0xFF])<<16 | uint32(sbox[(w>>8)&0xFF])<<8 | uint32(sbox[w&0xFF])
 }
 
-// Gets round constant
-// Pre: b > 0
-func rcon(b uint8) uint32 {
-	return uint32(rcon_const[b - 1]) << 24
+// Gets round constant.
+// Pre: round > 0
+func rcon(round int) uint32 {
+	return uint32(rcon_const[round - 1]) << 24
 }
