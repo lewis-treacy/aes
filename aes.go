427268d26161a1580f3b14f2d5ee9c67ef46859e
Lewis Treacy <lewis.treacy16@imperial.ac.uk>
2017-09-17 01:07:34 +0100

    Implemented round constant (rcon) for key expansion

--- aes.go
+++ aes.go
@@ -1,11 +1,23 @@
 // Advanced Encryption Standard implementation
 package aes
 
+func expandKey(key []byte) []byte {
+	var exKey = make([]byte, 256)
+	return exKey
+}
+
+// Rotate 32 bit word left by 1 byte
 func rotw(w uint32) uint32 {
 	return (w<<8) | (w>>24)
 }
 
+// Gets S-Box substitution value for a byte
 func sub(b byte) byte {
 	return sbox[uint8(b)]
 }
 
+// Gets round constant
+// Pre: b > 0
+func rcon(b uint8) uint32 {
+	return uint32(rcon_const[b - 1]) << 24
+}
--- aes_test.go
+++ aes_test.go
@@ -12,7 +12,7 @@ func Test_rotr(t *testing.T) {
 	}{
 		got := rotw(c.in)
 		if got != c.want {
-			t.Errorf("rotw(0x%X) == 0x%X, want 0x%X", c.in, got, c.want)
+			t.Errorf("rotw(0x%08X) == 0x%08X, want 0x%08X", c.in, got, c.want)
 		}
 	}
 }
@@ -28,7 +28,23 @@ func Test_sub(t *testing.T) {
 	}{
 		got := sub(c.in)
 		if got != c.want {
-			t.Errorf("sub(0x%X) == 0x%X, want 0x%X", c.in, got, c.want)
+			t.Errorf("sub(0x%02X) == 0x%02X, want 0x%02X", c.in, got, c.want)
+		}
+	}
+}
+
+func Test_rcon(t *testing.T) {
+	for _, c := range []struct {
+		in uint8
+		want uint32
+	}{
+		{0x01, 0x01000000},
+		{0x0E, 0x4D000000},
+		{0x0F, 0x9A000000},
+	}{
+		got := rcon(c.in)
+		if got != c.want {
+			t.Errorf("rcon(0x%02X) == 0x%08X, want 0x%08X", c.in, got, c.want)
 		}
 	}
 }
