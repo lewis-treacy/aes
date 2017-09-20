08d8f1c8023c172eeb19d2d9b72194763a311266
Lewis Treacy <lewis.treacy16@imperial.ac.uk>
2017-09-20 20:14:22 +0100

    implemented mix columns and changed representations from words to bytes

--- aes.go
+++ aes.go
@@ -3,78 +3,138 @@ package aes
 
 type key struct {
 	key []byte
-	xkey []uint32
+	xkey []byte
 	rounds int
 }
 
-func encryptBlock(b []byte, k *key) []byte {
-	s := make([]uint32, 4)
-	s[0] = uint32(b[0]) << 24 | uint32(b[1]) << 16 | uint32(b[2]) << 8 | uint32(b[3])
-	s[1] = uint32(b[4]) << 24 | uint32(b[5]) << 16 | uint32(b[6]) << 8 | uint32(b[7])
-	s[2] = uint32(b[8]) << 24 | uint32(b[9]) << 16 | uint32(b[10]) << 8 | uint32(b[11])
-	s[2] = uint32(b[12]) << 24 | uint32(b[13]) << 16 | uint32(b[14]) << 8 | uint32(b[15])
-
-	s = addRoundKey(s, k.xkey)
-	for i := 1; i < k.rounds; i++{
-		// Sub bytes
-		s[0] = subw(s[0])
-		s[1] = subw(s[1])
-		s[2] = subw(s[2])
-		s[3] = subw(s[3])
-		// Shift rows
-		s[1] = (s[1]<<8) | (s[1]>>24)
-		s[2] = (s[2]<<16) | (s[2]>>16)
-		s[3] = (s[3]<<24) | (s[3]>>8)
-		//TODO: Mix columns
-		s = addRoundKey(s, k.xkey[i*4:])
-	}
-
-	return []byte{0}
-}
-
-func addRoundKey(s []uint32, w []uint32) []uint32 {
-	s[0] ^= w[0]
-	s[1] ^= w[1]
-	s[2] ^= w[2]
-	s[3] ^= w[3]
-	return s
-}
-
-// Expands key according to the Rijndael key schedule.
-// Pre: key is 16, 24 or 32 bytes in length
-func expandKey(key []byte) []uint32 {
-	nk := len(key) / 4
-	w := make([]uint32, 4*(rounds[len(key)] + 1))
-	// Begining of the expanded key is the same
-	for i := 0; i < nk; i++ {
-		w[i] = uint32(key[4*i])<<24 | uint32(key[4*i+1])<<16 | uint32(key[4*i+2])<<8 | uint32(key[4*i+3])
-	}
-
-	for j := nk; j < len(w); j++ {
-		a := w[j-1]
-		if j % nk == 0 {
-			a = subw(rotw(a)) ^ rcon(j/nk)
-		} else if nk == 8 && j % nk == 4 {
-			a = subw(a)
-		}
-		w[j] = w[j-nk] ^ a
-	}
-
+// Rotate word left 1 byte
+func rotw(w []byte) []byte {
+	temp := w[0]
+	w[0] = w[1]
+	w[1] = w[2]
+	w[2] = w[3]
+	w[3] = temp
 	return w
 }
 
-// Rotate word left 1 byte.
-func rotw(w uint32) uint32 {
-	return (w<<8) | (w>>24)
+// Returns substitute byte according to Rijndael's S-box
+func getSboxSub(b byte) byte {
+	return sbox[b]
 }
 
-// Substitutes each byte of word w according to Rijndael's S-box.
-func subw(w uint32) uint32 {
-	return uint32(sbox[w>>24])<<24 | uint32(sbox[(w>>16)&0xFF])<<16 | uint32(sbox[(w>>8)&0xFF])<<8 | uint32(sbox[w&0xFF])
+// Returns round constant
+// Pre: 0 < round <= 10
+func getRcon(round int) byte {
+	return rcon_const[round - 1]
 }
 
-// Gets round constant.
-// Pre: round > 0
-func rcon(round int) uint32 {
-	return uint32(rcon_const[round - 1]) << 24
+// Expands key according to the Rijndael key schedule
+// Pre: key is 16, 24 or 32 bytes in length
+func expandKey(key []byte) []byte {
+	nk := len(key) / 4
+	xKey := make([]byte, 16*(rounds[len(key)] + 1))
+
+	// Begining of the expanded key is the same
+	i := 0
+	for ; i < nk; i++ {
+		xKey[(4*i)+0] = key[(4*i)+0]
+		xKey[(4*i)+1] = key[(4*i)+1]
+		xKey[(4*i)+2] = key[(4*i)+2]
+		xKey[(4*i)+3] = key[(4*i)+3]
+	}
+
+	temp := make([]byte, 4)
+	for ; i < len(xKey)/4; i++ {
+		temp[0] = xKey[4*(i-1) + 0]
+		temp[1] = xKey[4*(i-1) + 1]
+		temp[2] = xKey[4*(i-1) + 2]
+		temp[3] = xKey[4*(i-1) + 3]
+
+		if i % nk == 0 {
+			// Rotate word
+			temp = rotw(temp)
+			// Sub word
+			temp = subBytes(temp)
+			// Rcon
+			temp[0] = temp[0] ^ getRcon(i/nk)
+		} else if nk > 6 && i % nk == 4 {
+			// Sub word
+			temp = subBytes(temp)
+		}
+		xKey[(4*i) + 0] = xKey[4*(i-nk) + 0] ^ temp[0]
+		xKey[(4*i) + 1] = xKey[4*(i-nk) + 1] ^ temp[1]
+		xKey[(4*i) + 2] = xKey[4*(i-nk) + 2] ^ temp[2]
+		xKey[(4*i) + 3] = xKey[4*(i-nk) + 3] ^ temp[3]
+	}
+	return xKey
+}
+
+// Adds (xor) state to round key
+func addRoundKey(s []byte, w []byte) []byte {
+	t := make([]byte, 16)
+	for i, v := range s {
+		t[i] = v ^ w[i]
+	}
+	return t
+}
+
+// Returns state with each byte substituted according to Rijndael's S-box
+func subBytes(s []byte) []byte {
+	t := make([]byte, len(s))
+	for i, v := range s {
+		t[i] = getSboxSub(v)
+	}
+	return t
+}
+
+// Shifts rows to the right, by one more byte for each row
+func shiftRows(s []byte) []byte {
+	t := make([]byte, 16)
+	for i := 0; i < 4; i++ {
+		t[i] = s[5*i]
+		t[4+i] = s[(4+(5*i)) % 16]
+		t[8+i] = s[(8+(5*i)) % 16]
+		t[12+i] = s[(12+(5*i)) % 16]
+	}
+	return t
+}
+
+func mixColumns(s []byte) []byte {
+	t := make([]byte, 4)
+	temp := make([]byte, 16)
+
+	for i := 0; i < 4; i++ {
+		t[0] = gf_mul2[s[(4*i) + 0]] ^ s[(4*i) + 3] ^ s[(4*i) + 2] ^ gf_mul3[s[(4*i) + 1]] // 2*a0 + a3 + a2 + 3*a1
+		t[1] = gf_mul2[s[(4*i) + 1]] ^ s[(4*i) + 0] ^ s[(4*i) + 3] ^ gf_mul3[s[(4*i) + 2]] // 2*a1 + a0 + a3 + 3*a2
+		t[2] = gf_mul2[s[(4*i) + 2]] ^ s[(4*i) + 1] ^ s[(4*i) + 0] ^ gf_mul3[s[(4*i) + 3]] // 2*a2 + a1 + a0 + 3*a3
+		t[3] = gf_mul2[s[(4*i) + 3]] ^ s[(4*i) + 2] ^ s[(4*i) + 1] ^ gf_mul3[s[(4*i) + 0]] // 2*a3 + a2 + a1 + 3*a0
+		temp[(4*i) + 0] = t[0]
+		temp[(4*i) + 1] = t[1]
+		temp[(4*i) + 2] = t[2]
+		temp[(4*i) + 3] = t[3]
+	}
+	return temp
+}
+
+func encryptBlock(b []byte, k *key) []byte {
+	// Add round key
+	s := addRoundKey(b, k.xkey)
+	for i := 1; i < k.rounds; i++{
+		// Sub bytes
+		s = subBytes(s)
+		// Shift rows, wrong
+		s = shiftRows(s)
+		// Mix columns
+		s = mixColumns(s)
+		// Add round key
+		s = addRoundKey(s, k.xkey[i*16:])
+	}
+
+	// Sub bytes
+	s = subBytes(s)
+	// Shift rows
+	s = shiftRows(s)
+	// Add round key
+	s = addRoundKey(s, k.xkey[len(k.xkey)-16:])
+	return s
 }
