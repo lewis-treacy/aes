97a9da952643e226d6baf7e42e751f68186d19e9
Lewis Treacy <lewis.treacy16@imperial.ac.uk>
2017-09-20 21:42:31 +0100

    style fixes

--- aes.go
+++ aes.go
@@ -2,8 +2,8 @@
 package aes
 
 type key struct {
-	key []byte
-	xkey []byte
+	key    []byte
+	xkey   []byte
 	rounds int
 }
 
@@ -31,14 +31,14 @@ func subBytes(s []byte) {
 // Returns round constant
 // Pre: 0 < round <= 10
 func getRcon(round int) byte {
-	return rcon_const[round - 1]
+	return rcon_const[round-1]
 }
 
 // Expands key according to the Rijndael key schedule
 // Pre: key is 16, 24 or 32 bytes in length
 func expandKey(key []byte) []byte {
 	nk := len(key) / 4
-	xKey := make([]byte, 16*(rounds[len(key)] + 1))
+	xKey := make([]byte, 16*(rounds[len(key)]+1))
 
 	// Begining of the expanded key is the same
 	i := 0
@@ -51,26 +51,26 @@ func expandKey(key []byte) []byte {
 
 	temp := make([]byte, 4)
 	for ; i < len(xKey)/4; i++ {
-		temp[0] = xKey[4*(i-1) + 0]
-		temp[1] = xKey[4*(i-1) + 1]
-		temp[2] = xKey[4*(i-1) + 2]
-		temp[3] = xKey[4*(i-1) + 3]
+		temp[0] = xKey[4*(i-1)+0]
+		temp[1] = xKey[4*(i-1)+1]
+		temp[2] = xKey[4*(i-1)+2]
+		temp[3] = xKey[4*(i-1)+3]
 
-		if i % nk == 0 {
+		if i%nk == 0 {
 			// Rotate word
 			rotw(temp)
 			// Sub word
 			subBytes(temp)
 			// Rcon
 			temp[0] = temp[0] ^ getRcon(i/nk)
-		} else if nk > 6 && i % nk == 4 {
+		} else if nk > 6 && i%nk == 4 {
 			// Sub word
 			subBytes(temp)
 		}
-		xKey[(4*i) + 0] = xKey[4*(i-nk) + 0] ^ temp[0]
-		xKey[(4*i) + 1] = xKey[4*(i-nk) + 1] ^ temp[1]
-		xKey[(4*i) + 2] = xKey[4*(i-nk) + 2] ^ temp[2]
-		xKey[(4*i) + 3] = xKey[4*(i-nk) + 3] ^ temp[3]
+		xKey[(4*i)+0] = xKey[4*(i-nk)+0] ^ temp[0]
+		xKey[(4*i)+1] = xKey[4*(i-nk)+1] ^ temp[1]
+		xKey[(4*i)+2] = xKey[4*(i-nk)+2] ^ temp[2]
+		xKey[(4*i)+3] = xKey[4*(i-nk)+3] ^ temp[3]
 	}
 	return xKey
 }
@@ -87,9 +87,9 @@ func shiftRows(s []byte) {
 	t := make([]byte, 4)
 	for i := 0; i < 4; i++ {
 		t[0] = s[5*i]
-		t[1] = s[((5*i)+4) % 16]
-		t[2] = s[((5*i)+8) % 16]
-		t[3] = s[((5*i)+12) % 16]
+		t[1] = s[((5*i)+4)%16]
+		t[2] = s[((5*i)+8)%16]
+		t[3] = s[((5*i)+12)%16]
 
 		s[i] = t[0]
 		s[4+i] = t[1]
@@ -101,22 +101,22 @@ func shiftRows(s []byte) {
 func mixColumns(s []byte) {
 	t := make([]byte, 4)
 	for i := 0; i < 4; i++ {
-		t[0] = gf_mul2[s[(4*i) + 0]] ^ s[(4*i) + 3] ^ s[(4*i) + 2] ^ gf_mul3[s[(4*i) + 1]] // 2*a0 + a3 + a2 + 3*a1
-		t[1] = gf_mul2[s[(4*i) + 1]] ^ s[(4*i) + 0] ^ s[(4*i) + 3] ^ gf_mul3[s[(4*i) + 2]] // 2*a1 + a0 + a3 + 3*a2
-		t[2] = gf_mul2[s[(4*i) + 2]] ^ s[(4*i) + 1] ^ s[(4*i) + 0] ^ gf_mul3[s[(4*i) + 3]] // 2*a2 + a1 + a0 + 3*a3
-		t[3] = gf_mul2[s[(4*i) + 3]] ^ s[(4*i) + 2] ^ s[(4*i) + 1] ^ gf_mul3[s[(4*i) + 0]] // 2*a3 + a2 + a1 + 3*a0
+		t[0] = gf_mul2[s[(4*i)+0]] ^ s[(4*i)+3] ^ s[(4*i)+2] ^ gf_mul3[s[(4*i)+1]] // 2*a0 + a3 + a2 + 3*a1
+		t[1] = gf_mul2[s[(4*i)+1]] ^ s[(4*i)+0] ^ s[(4*i)+3] ^ gf_mul3[s[(4*i)+2]] // 2*a1 + a0 + a3 + 3*a2
+		t[2] = gf_mul2[s[(4*i)+2]] ^ s[(4*i)+1] ^ s[(4*i)+0] ^ gf_mul3[s[(4*i)+3]] // 2*a2 + a1 + a0 + 3*a3
+		t[3] = gf_mul2[s[(4*i)+3]] ^ s[(4*i)+2] ^ s[(4*i)+1] ^ gf_mul3[s[(4*i)+0]] // 2*a3 + a2 + a1 + 3*a0
 
-		s[(4*i) + 0] = t[0]
-		s[(4*i) + 1] = t[1]
-		s[(4*i) + 2] = t[2]
-		s[(4*i) + 3] = t[3]
+		s[(4*i)+0] = t[0]
+		s[(4*i)+1] = t[1]
+		s[(4*i)+2] = t[2]
+		s[(4*i)+3] = t[3]
 	}
 }
 
 func encryptBlock(s []byte, k *key) {
 	// Add round key
 	addRoundKey(s, k.xkey)
-	for i := 1; i < k.rounds; i++{
+	for i := 1; i < k.rounds; i++ {
 		// Sub bytes
 		subBytes(s)
 		// Shift rows, wrong
